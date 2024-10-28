b630092c9135ab0a097d0b86c41dd34dabe8919b
Ana Cecilia Porcidonio <cecilia.pordidonio@gmail.com>
2024-10-28 17:54:46 -0300

    first commit

---
+++ springboot/src/test/java/com/seu_usuario/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seu_usuario.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
